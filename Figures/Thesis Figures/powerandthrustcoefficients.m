de23b32566a2dd21010e885df8ef5f704edfdc19
Noah Miller <nsm0014@auburn.edu>
2023-07-25 15:46:48 -0500

    wrapping up propeller section

---
+++ Figures/Thesis Figures/powerandthrustcoefficients.m
@@ -0,0 +1,20 @@
+%% Formatting
+clc
+clear
+close all
+format shortg
+
+%% Defining Constants
+AF = 101.89;
+CL = 0.5;
+D = 1.9;
+n = 36.6;
+rho = 1.225;
+P = 447.42;
+CP = 0:0.05:0.5;
+
+for i = 1:numel(CP)
+
+    
+
+end
