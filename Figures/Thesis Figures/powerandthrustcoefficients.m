59c5bd4b8701a6f84eda999d21477c8044f45c83
Noah Miller <nsm0014@auburn.edu>
2023-07-26 16:21:47 -0500

    added CTCP plot

--- Figures/Thesis Figures/powerandthrustcoefficients.m
+++ Figures/Thesis Figures/powerandthrustcoefficients.m
@@ -4,17 +4,25 @@ clear
 close all
 format shortg
 
-%% Defining Constants
-AF = 101.89;
-CL = 0.5;
-D = 1.9;
-n = 36.6;
-rho = 1.225;
-P = 447.42;
-CP = 0:0.05:0.5;
+%% Reading in CSV file
+data = readtable("digitizedstaticthrust.csv","VariableNamingRule","preserve");
 
-for i = 1:numel(CP)
+CP = (447.42*550)/(1.225*36.6^3*1.9^5)
+CPCT = interp1(data.x,data.("CL = 0.500"),CP)
 
-    
-
-end
+figure('Position',[450 450 900 600])
+hold on
+grid on
+% plot(data.x,data.("CL = 0.700"),'LineWidth',2,'Color',[0 0 0],'Marker','+')
+% plot(data.x,data.Ideal,'LineWidth',1.5,'Color',[0 0 0],'Marker','+')
+plot(data.x,data.("CL = 0.500"),'LineWidth',2,'Color',[0 0 0])
+scatter(CP,CPCT,125,'red','o','filled')
+% plot(data.x,data.("CL = 0.300"),'LineWidth',2,'Color',[0 0 0],'Marker','square')
+% plot(data.x,data.("CL = 0.150"),'LineWidth',2,'Color',[0 0 0],'Marker','o')
+xlabel('C_P')
+ylabel('C_T/C_P')
+legend('Blade Design C_L = 0.5','Design Point')
+ylim([0 3.25])
+xlim([0 0.32])
+ax = gca;
+ax.FontSize = 16;
